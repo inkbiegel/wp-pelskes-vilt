06c33d46e31a43ed9c2d315d3a065674b7305356
git <jellevuylsteke@gmail.com>
2019-01-02 18:53:47 +0100

    removed custom template tags

--- wp-content/themes/pelske/functions.php
+++ wp-content/themes/pelske/functions.php
@@ -137,11 +137,6 @@ add_action( 'wp_enqueue_scripts', 'pelske_scripts' );
 require get_template_directory() . '/inc/custom-header.php';
 
 /**
- * Custom template tags for this theme.
- */
-require get_template_directory() . '/inc/template-tags.php';
-
-/**
  * Functions which enhance the theme by hooking into WordPress.
  */
 require get_template_directory() . '/inc/template-functions.php';
