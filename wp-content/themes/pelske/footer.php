c740a6450b9dc10361c0d93c2f5374c84d9080e9
inkbiegel <jellevuylsteke@gmail.com>
2020-05-29 18:55:58 +0200

    moved social icons back to sprite, added close btn

--- wp-content/themes/pelske/footer.php
+++ wp-content/themes/pelske/footer.php
@@ -13,34 +13,42 @@
 	<footer id="colophon" class="site-footer">
 		<ul class="icon-list txt-center">
 			<li class="icon-list-item">
-				<a href="https://www.facebook.com/pelske.elske" target="_social" class="icon-svg icon-social" id="iconSocialFb">
+				<a href="https://www.facebook.com/pelske.elske" target="_social" class="icon-svg icon-social icon-anim-border" id="iconSocialFb">
 					<svg role="img" title="Follow me on Facebook" aria-labelled-by="iconFacebookTitle" viewBox="0 0 48 48">
 						<title id="iconFacebookTitle">Follow me on Facebook</title>
-						<path d="M22.3,41.6L22.3,41.6L22.3,41.6L22.3,41.6z M21.2,41.5C12.8,40.2,6.3,32.8,6.3,24 c0-9.8,8-17.7,17.7-17.7s17.7,8,17.7,17.7c0,8.8-6.4,16.2-14.9,17.5V29H31l0.8-5.2h-5v-3.4c0-1.4,0.7-2.8,2.9-2.8H32v-4.2	c0,0-2.1-0.3-3.9-0.3c-4,0-6.7,2.5-6.7,6.8V24h-4.5v5.2h4.3V41.5z"/>
+						<use xlink:href="#icon-facebook"></use>
 						<circle class="border" fill="none" stroke-miterlimit="10" cx="24" cy="24" r="22.4" transform="rotate(-180 24 24)"/>
 					</svg>
 				</a>
 			</li>
 			<li class="icon-list-item">
-				<a href="https://www.instagram.com/pelske.elske" target="_social" class="icon-svg icon-social" id="iconSocialInstagram">
+				<a href="https://www.instagram.com/pelske.elske" target="_social" class="icon-svg icon-social icon-anim-border" id="iconSocialInstagram">
 					<svg role="img" title="Follow me on Instagram" aria-labelled-by="iconInstagramTitle" viewBox="0 0 48 48">
 						<title id="iconInstagramTitle">Follow me on Instagram</title>
-						<path d="M24,6.3c10,0,18.1,8.1,18.1,18.1S34,42.6,24,42.6 S5.9,34.4,5.9,24.4S14,6.3,24,6.3z M24,13.1c-3.1,0-3.5,0-4.7,0.1c-1.2,0.1-2,0.2-2.8,0.5c-0.7,0.3-1.4,0.7-2,1.3	c-0.6,0.6-1,1.3-1.3,2c-0.3,0.7-0.5,1.5-0.5,2.8c-0.1,1.2-0.1,1.6-0.1,4.7s0,3.5,0.1,4.7c0.1,1.2,0.2,2,0.5,2.8	c0.3,0.7,0.7,1.4,1.3,2c0.6,0.6,1.3,1,2,1.3c0.7,0.3,1.5,0.5,2.8,0.5c1.2,0.1,1.6,0.1,4.7,0.1s3.5,0,4.7-0.1c1.2-0.1,2-0.2,2.8-0.5 c0.7-0.3,1.4-0.7,2-1.3c0.6-0.6,1-1.3,1.3-2c0.3-0.7,0.5-1.5,0.5-2.8c0.1-1.2,0.1-1.6,0.1-4.7s0-3.5-0.1-4.7	c-0.1-1.2-0.2-2-0.5-2.8c-0.3-0.7-0.7-1.4-1.3-2c-0.6-0.6-1.3-1-2-1.3c-0.7-0.3-1.5-0.5-2.8-0.5C27.5,13.1,27.1,13.1,24,13.1z M24,15.1c3,0,3.4,0,4.6,0.1c1.1,0.1,1.7,0.2,2.1,0.4c0.5,0.2,0.9,0.5,1.3,0.8c0.4,0.4,0.6,0.8,0.8,1.3c0.2,0.4,0.3,1,0.4,2.1 c0.1,1.2,0.1,1.6,0.1,4.6s0,3.4-0.1,4.6c-0.1,1.1-0.2,1.7-0.4,2.1c-0.2,0.5-0.5,0.9-0.8,1.3c-0.4,0.4-0.8,0.6-1.3,0.8 c-0.4,0.2-1,0.3-2.1,0.4c-1.2,0.1-1.6,0.1-4.6,0.1s-3.4,0-4.6-0.1c-1.1-0.1-1.7-0.2-2.1-0.4c-0.5-0.2-0.9-0.5-1.3-0.8 c-0.4-0.4-0.6-0.8-0.8-1.3c-0.2-0.4-0.3-1-0.4-2.1c-0.1-1.2-0.1-1.6-0.1-4.6s0-3.4,0.1-4.6c0.1-1.1,0.2-1.7,0.4-2.1 c0.2-0.5,0.5-0.9,0.8-1.3c0.4-0.4,0.8-0.6,1.3-0.8c0.4-0.2,1-0.3,2.1-0.4C20.6,15.1,21,15.1,24,15.1z M24,18.6	c-3.2,0-5.8,2.6-5.8,5.8c0,3.2,2.6,5.8,5.8,5.8s5.8-2.6,5.8-5.8S27.2,18.6,24,18.6z M24,28.2c-2.1,0-3.8-1.7-3.8-3.8 s1.7-3.8,3.8-3.8s3.8,1.7,3.8,3.8S26.1,28.2,24,28.2z M31.4,18.4c0,0.8-0.6,1.4-1.4,1.4c-0.8,0-1.4-0.6-1.4-1.4s0.6-1.4,1.4-1.4 C30.8,17,31.4,17.6,31.4,18.4z"/>
+						<use xlink:href="#icon-instagram"></use>
 						<circle class="border" fill="none" stroke-miterlimit="10" cx="24" cy="24" r="22.4" transform="rotate(-180 24 24)"/>
 					</svg>
 				</a>
 			</li>
 			<li class="icon-list-item">
-				<a href="https://www.pinterest.com/pelske/" target="_social" class="icon-svg icon-social" id="iconSocialPinterest">
+				<a href="https://www.pinterest.com/pelske/" target="_social" class="icon-svg icon-social icon-anim-border" id="iconSocialPinterest">
 					<svg role="img" title="Follow me on Pinterest" aria-labelled-by="iconPinterestTitle" viewBox="0 0 48 48">
 						<title id="iconPinterestTitle">Follow me on Pinterest</title>
-						<path d="M24,6.2c-9.8,0-17.7,7.9-17.7,17.7c0,7.5,4.7,13.9,11.3,16.5c-0.2-1.4-0.3-3.6,0.1-5.1 c0.3-1.4,2.1-8.8,2.1-8.8s-0.5-1.1-0.5-2.6c0-2.5,1.4-4.3,3.2-4.3c1.5,0,2.2,1.1,2.2,2.5c0,1.5-1,3.8-1.5,5.9	c-0.4,1.8,0.9,3.2,2.6,3.2c3.2,0,5.6-3.3,5.6-8.1c0-4.2-3-7.2-7.4-7.2c-5,0-8,3.8-8,7.7c0,1.5,0.6,3.2,1.3,4 c0.1,0.2,0.2,0.3,0.1,0.5c-0.1,0.6-0.4,1.8-0.5,2c-0.1,0.3-0.3,0.4-0.6,0.2c-2.2-1-3.6-4.3-3.6-6.9c0-5.6,4.1-10.7,11.7-10.7 c6.1,0,10.9,4.4,10.9,10.2c0,6.1-3.9,11-9.2,11c-1.8,0-3.5-0.9-4.1-2c0,0-0.9,3.4-1.1,4.2c-0.4,1.5-1.5,3.5-2.2,4.6	c1.7,0.5,3.4,0.8,5.2,0.8c9.8,0,17.7-7.9,17.7-17.7C41.7,14.1,33.8,6.2,24,6.2z"/>						<circle class="border" fill="none" stroke-miterlimit="10" cx="24" cy="24" r="22.4" transform="rotate(-180 24 24)"/>
+						<use xlink:href="#icon-pinterest"></use>
+						<circle class="border" fill="none" stroke-miterlimit="10" cx="24" cy="24" r="22.4" transform="rotate(-180 24 24)"/>
 					</svg>
 				</a>
 			</li>
 		</ul>
 	</footer>
 	<div class="overlay" id="overlay">
+		<button class="icon-svg icon-anim-border btn-close" id="overlayBtnClose">
+			<svg role="img" title="Close" aria-labelled-by="btnCloseTitle" viewBox="0 0 48 48">
+				<title id="btnCloseTitle">Close</title>
+				<use xlink:href="#btn-close"></use>
+				<circle class="border" fill="none" stroke-miterlimit="10" cx="24" cy="24" r="22.4" transform="rotate(-180 24 24)"/>
+			</svg>
+		</button>
 		<div class="site-logo"><?php get_template_part( 'template-parts/logo-pelskes-vilt_circle.svg' ); ?></div>
 		<div class="overlay-grid"></div>
 	</div>
