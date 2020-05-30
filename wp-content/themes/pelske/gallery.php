bb1654c79294a0f1ce6fc34080df78885a390cbc
inkbiegel <jellevuylsteke@gmail.com>
2020-05-30 15:25:36 +0200

    gallery tweaks

--- wp-content/themes/pelske/gallery.php
+++ wp-content/themes/pelske/gallery.php
@@ -58,7 +58,7 @@ get_header();
 				$full_img = wp_get_attachment_image_src( get_field('gallery_img_large'), 'full' );
 		?>
 			<li class="grid-list-item gallery-item">
-				<a class="grid-list-link" href="<?php echo $full_img[0]; ?>">
+				<a class="grid-list-link gallery-link" href="<?php echo $full_img[0]; ?>">
 					<?php
 						$image = get_field('gallery_img_small');
 						echo wp_get_attachment_image( $image, 'medium', false, array( 'class' => 'grid-list-img gallery-img-small' ) );
