852e6974aa05851b1b18aefbf225b5c2b54c2f00
inkbiegel <jellevuylsteke@gmail.com>
2020-06-27 19:33:47 +0200

    split content in 3 blocks with images

--- wp-content/themes/pelske/static.php
+++ wp-content/themes/pelske/static.php
@@ -6,12 +6,24 @@
  */
 
 get_header();
+$n_text_blocks = 3;
 ?>
 	<main id="main" class="site-main">
-		<?php
-		 the_title('<h1><span>','</span></h1>');
-		 the_content();
-		?>
+		<article>
+			<?php
+				esc_html( the_title('<h1><span>','</span></h1>') );
+				for ($i=1; $i < $n_text_blocks+1; $i++) {
+					$text = get_field('block_' . $i . '_text');
+					$text = apply_filters('the_content', $text);
+					echo '<section class="text-block">' .
+									'<div class="text-block-img-wrap">' .
+										wp_get_attachment_image( get_field('block_' . $i . '_img'), 'large', false, array( 'class' => 'text-block-img' ) ) .
+									'</div>' .
+									'<div class="text-block-copy">' . $text . '</div>' .
+								'</section>';
+				}
+			?>
+		</article>
 	</main>
 <?php
 get_footer();
