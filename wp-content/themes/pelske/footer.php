18d79f4f8847797e6944c7c1048ce541f0cb42fa
inkbiegel <jellevuylsteke@gmail.com>
2020-07-09 15:06:40 +0200

    fixed aria label spelling

--- wp-content/themes/pelske/footer.php
+++ wp-content/themes/pelske/footer.php
@@ -14,7 +14,7 @@
 		<ul class="icon-list txt-center">
 			<li class="icon-list-item">
 				<a href="https://www.facebook.com/pelske.elske" target="_social" class="icon-svg icon-social icon-anim-border" id="iconSocialFb">
-					<svg role="img" title="Follow me on Facebook" aria-labelled-by="iconFacebookTitle" viewBox="0 0 48 48">
+					<svg role="img" title="Follow me on Facebook" aria-labelledby="iconFacebookTitle" viewBox="0 0 48 48">
 						<title id="iconFacebookTitle"><?php _e('Follow me on Facebook', 'pelske'); ?></title>
 						<use xlink:href="#icon-facebook"></use>
 						<circle class="border" fill="none" stroke-miterlimit="10" cx="24" cy="24" r="22.4" transform="rotate(-180 24 24)"/>
@@ -23,7 +23,7 @@
 			</li>
 			<li class="icon-list-item">
 				<a href="https://www.instagram.com/pelske.elske" target="_social" class="icon-svg icon-social icon-anim-border" id="iconSocialInstagram">
-					<svg role="img" title="Follow me on Instagram" aria-labelled-by="iconInstagramTitle" viewBox="0 0 48 48">
+					<svg role="img" title="Follow me on Instagram" aria-labelledby="iconInstagramTitle" viewBox="0 0 48 48">
 						<title id="iconInstagramTitle"><?php _e('Follow me on Instagram', 'pelske'); ?></title>
 						<use xlink:href="#icon-instagram"></use>
 						<circle class="border" fill="none" stroke-miterlimit="10" cx="24" cy="24" r="22.4" transform="rotate(-180 24 24)"/>
@@ -32,7 +32,7 @@
 			</li>
 			<li class="icon-list-item">
 				<a href="https://www.pinterest.com/pelske/" target="_social" class="icon-svg icon-social icon-anim-border" id="iconSocialPinterest">
-					<svg role="img" title="Follow me on Pinterest" aria-labelled-by="iconPinterestTitle" viewBox="0 0 48 48">
+					<svg role="img" title="Follow me on Pinterest" aria-labelledby="iconPinterestTitle" viewBox="0 0 48 48">
 						<title id="iconPinterestTitle"><?php _e('Follow me on Pinterest', 'pelske'); ?></title>
 						<use xlink:href="#icon-pinterest"></use>
 						<circle class="border" fill="none" stroke-miterlimit="10" cx="24" cy="24" r="22.4" transform="rotate(-180 24 24)"/>
@@ -51,7 +51,7 @@
 	</footer>
 	<div class="overlay on-load" id="overlay">
 		<button class="icon-svg icon-anim-border btn-close" id="overlayBtnClose">
-			<svg role="img" title="Close" aria-labelled-by="btnCloseTitle" viewBox="0 0 48 48">
+			<svg role="img" title="Close" aria-labelledby="btnCloseTitle" viewBox="0 0 48 48">
 				<title id="btnCloseTitle"><?php _e('Close', 'pelske'); ?></title>
 				<use xlink:href="#btn-close"></use>
 				<circle class="border" fill="none" stroke-miterlimit="10" cx="24" cy="24" r="22.4" transform="rotate(-180 24 24)"/>
@@ -65,7 +65,7 @@
 		<ul class="icon-list icon-list__vertical">
 			<li class="icon-list-item">
 				<a class="icon-svg icon-social icon-anim-border" id="btnSharePinterest" href="https://www.pinterest.com/pin/create/button/?url=<?php echo $page_url; ?>&media=IMGURL&description=<?php echo $share_text; ?>" target="_social">
-					<svg role="img" title="<?php _e('Pin on Pinterest', 'pelske'); ?>" aria-labelled-by="btnPinterestTitle" viewBox="0 0 48 48">
+					<svg role="img" title="<?php _e('Pin on Pinterest', 'pelske'); ?>" aria-labelledby="btnPinterestTitle" viewBox="0 0 48 48">
 						<title id="btnPinterestTitle"><?php _e('Pin on Pinterest', 'pelske'); ?></title>
 						<use xlink:href="#icon-pinterest"></use>
 						<circle class="border" fill="none" stroke-miterlimit="10" cx="24" cy="24" r="22.4" transform="rotate(-180 24 24)"/>
@@ -74,7 +74,7 @@
 			</li>
 			<li class="icon-list-item">
 				<a class="icon-svg icon-social icon-anim-border" id="btnShareFacebook" href="https://www.facebook.com/sharer.php?u=<?php echo $page_url; ?>" target="_social">
-					<svg role="img" title="<?php _e('Share on Facebook', 'pelske'); ?>" aria-labelled-by="btnFacebookTitle" viewBox="0 0 48 48">
+					<svg role="img" title="<?php _e('Share on Facebook', 'pelske'); ?>" aria-labelledby="btnFacebookTitle" viewBox="0 0 48 48">
 						<title id="btnFacebookTitle"><?php _e('Share on Facebook', 'pelske'); ?></title>
 						<use xlink:href="#icon-facebook"></use>
 						<circle class="border" fill="none" stroke-miterlimit="10" cx="24" cy="24" r="22.4" transform="rotate(-180 24 24)"/>
